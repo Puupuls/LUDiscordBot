7fc6a7ad36a71486a4faabe4689e6c04b67d50ab
Puupuls <paulspurvins@gmail.com>
2022-11-21 20:09:13 +0200

    Updates for a new host

--- startup.sh
+++ startup.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
 screen -dmS LU_gaming_bot bash -c 'exec bash'
-screen -r LU_gaming_bot -X stuff "python3.9 '/home/pi/Desktop/LU_BOT/bot.py' \n"
+screen -r LU_gaming_bot -X stuff "mamba activate LU_BOT \n"
+screen -r LU_gaming_bot -X stuff "python3.9 '/mnt/data/LU_BOT/bot.py' \n"
